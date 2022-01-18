4db758449bf0f99cde7c1d3b6051e0f0badf0f8d
Alejandro Pugliesi <apugliesi@amherst.com>
2022-01-17 20:43:35 -0400

    Changing QA.UI for a demo of devextreme

---
+++ QA.UI/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Owner from './components/Owner';
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import './App.css';
+
+function App() {
+  return (
+    <Owner />
+  );
+}
+
+export default App;
--- QA.UI/src/index.js
+++ QA.UI/src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import repositoryReducer from './store/reducers/repositoryReducer';
-import errorHandlerReducer from './store/reducers/errorHandlerReducer';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootReducers = combineReducers({
-    repository: repositoryReducer,
-    errorHandler: errorHandlerReducer
-})
-
-const store = createStore(rootReducers, applyMiddleware(thunk));
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
